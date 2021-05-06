31759e2cfd6bfd9f745c7443edc1a293d0dfd4fe
Liam Whan <liam@vital.ly>
2021-05-07 09:02:54 +1000

    Add config for testing

---
+++ src/soxr-config.h
@@ -0,0 +1,13 @@
+#if !defined soxr_config_included
+#define soxr_config_included
+#define HAVE_STDBOOL_H
+#define HAVE_STDINT_H
+#define HAVE_LRINT
+#define WITH_CR32
+#define WITH_CR32S
+#define WITH_CR64
+#define WITH_CR64S
+#define WITH_VR32
+
+
+#endif
